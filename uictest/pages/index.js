98854ae6e4a0968e70266efb25e2beb32cba60e5
Jesse Parker <jessep6072@gmail.com>
2020-09-08 14:52:41 -0400

    initialize git repo for UICTEST project

---
+++ uictest/pages/index.js
@@ -0,0 +1,13 @@
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  
+  var outputString = "Grant hates Obamacare."
+  
+  return (
+    <div>
+      <h2>{outputString}</h2>
+    </div>
+  )
+}
