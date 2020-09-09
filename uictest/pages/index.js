ba0c80fd0f1a26978ef19deaf788fd2e0b60c6f9
jessep6072 <jessep6072@gmail.com>
2020-09-09 10:48:35 -0400

    initialize project

--- uictest/pages/index.js
+++ uictest/pages/index.js
@@ -1,13 +1,24 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+const db = require('../lib/db')
+const escape = require('sql-template-strings')
+
+module.exports = async (req, res) => {
+    
+  var result = await db.query(escape`
+    SELECT *
+    FROM UICDATA
+  `)
+  res.status(200).json(result)
+}
 
 export default function Home() {
   
-  var outputString = "Grant hates Obamacare."
+  //var outputString = "Grant hates Obamacare."
   
   return (
     <div>
-      <h2>{outputString}</h2>
+      <h2>{result}</h2>
     </div>
   )
 }
